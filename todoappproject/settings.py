d7558f509ddd2c711c66ece9aed81d41e0054c69
Vasil <58910106+vasetousa@users.noreply.github.com>
2022-06-06 22:07:42 +0200

    fixed settings

--- todoappproject/settings.py
+++ todoappproject/settings.py
@@ -133,8 +133,8 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = (BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
